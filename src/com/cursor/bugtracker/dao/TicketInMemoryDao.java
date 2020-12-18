10cd5461b5ebe6dc4be82e6d38d3ab8757b4e860
Dmytro <65898747+fill19@users.noreply.github.com>
2020-12-18 16:32:37 +0200

    Update TicketInMemoryDao.java

--- src/com/cursor/bugtracker/dao/TicketInMemoryDao.java
+++ src/com/cursor/bugtracker/dao/TicketInMemoryDao.java
@@ -8,55 +8,128 @@ import com.cursor.bugtracker.service.TicketService;
 
 import java.util.*;
 
-public class TicketInMemoryDao implements TicketDao, Singleton {
-    private static TicketInMemoryDao instance;
+public class TicketInMemoryDao {
+    static private TicketInMemoryDao instance;
 
     public static TicketInMemoryDao getInstance() {
-        if(instance == null)
+        if (instance == null) {
             instance = new TicketInMemoryDao();
+        }
         return instance;
     }
 
-    private Map<String, Ticket> tickets = new HashMap<>();
 
-    public TicketInMemoryDao() {
-        String id1 = UUID.randomUUID().toString();
-        Ticket ticket1 = new Ticket(
-                id1,
-                "Create app",
-                "Description of the god damn task",
-                new ArrayList<>(),
-                "5a817bdd-8294-4b05-a8f6-dc0e631563e5",
-                Status.TODO,
-                Priority.MEDIUM,
-                18000, // 5 hours
-                5000
-                );
+    public class TicketIn implements TicketDao {
 
-        tickets.put(id1, ticket1);
-    }
+        private Map<String, Ticket> tickets = new HashMap<>();
 
-    @Override
-    public Ticket save(Ticket ticket) {
-        tickets.put(ticket.getTicketId(), ticket);
-        return ticket;
-    }
+        public TicketIn() {
+            String id1 = UUID.randomUUID().toString();
+            Ticket ticket1 = new Ticket(
+                    id1,
+                    "Create app",
+                    "Description of the god damn task",
+                    new ArrayList<>(),
+                    "5a817bdd-8294-4b05-a8f6-dc0e631563e5",
+                    Status.TODO,
+                    Priority.MEDIUM,
+                    18000, // 5 hours
+                    5000
 
-    @Override
-    public Ticket getById(String ticketId) {
-        return tickets.get(ticketId);
-    }
+            );
+            String id2 = UUID.randomUUID().toString();
+            Ticket ticket2 = new Ticket(
+                    id1,
+                    "Create app",
+                    "Description of the god damn task",
+                    new ArrayList<>(),
+                    "7c2a397b-3634-426f-bf57-4a9bda5d6ede",
+                    Status.TODO,
+                    Priority.MEDIUM,
+                    18000, // 5 hours
+                    5000
 
-    @Override
-    public boolean removeById(String ticketId) {
+            );
+            String id3 = UUID.randomUUID().toString();
+            Ticket ticket3 = new Ticket(
+                    id1,
+                    "Create app",
+                    "Description of the god damn task",
+                    new ArrayList<>(),
+                    "4d87fd3a-ae9c-4834-a166-c745a8c92cda",
+                    Status.TODO,
+                    Priority.MEDIUM,
+                    18000, // 5 hours
+                    5000
 
-    }
+            );
+            String id4 = UUID.randomUUID().toString();
+            Ticket ticket4 = new Ticket(
+                    id1,
+                    "Create app",
+                    "Description of the god damn task",
+                    new ArrayList<>(),
+                    "9e7c6150-54f4-4f9e-a706-1412d65eb03a",
+                    Status.TODO,
+                    Priority.MEDIUM,
+                    18000, // 5 hours
+                    5000
 
-    public List<Ticket> findByName(String query) {
-        final List<Ticket> result = new LinkedList<>();
+            );
+            String id5 = UUID.randomUUID().toString();
+            Ticket ticket5 = new Ticket(
+                    id1,
+                    "Create app",
+                    "Description of the god damn task",
+                    new ArrayList<>(),
+                    "be3c0e68-d35c-4960-9c88-82f789bbcd06",
+                    Status.TODO,
+                    Priority.MEDIUM,
+                    18000, // 5 hours
+                    5000
 
-        //// find tickets
+            );
 
-        return result;
+
+            tickets.put(id1, ticket1);
+            tickets.put(id2, ticket2);
+            tickets.put(id3, ticket3);
+            tickets.put(id4, ticket4);
+            tickets.put(id5, ticket5);
+
+        }
+
+        @Override
+        public Ticket save(Ticket ticket) {
+            tickets.put(ticket.getTicketId(), ticket);
+            return ticket;
+        }
+
+        @Override
+        public Ticket getById(String ticketId) {
+            return tickets.get(ticketId);
+        }
+
+        @Override
+        public boolean removeById(String ticketId) {
+             tickets.remove(ticketId);
+            return Boolean.parseBoolean(ticketId);
+
+        }
+
+        public List<Ticket> findByName(String query) {
+            final List<Ticket> result = new LinkedList<>();
+            for (Ticket tickets : result) {
+                if (tickets.getTicketId().equals(query)) {
+                    return result;
+                }
+                //// find tickets
+
+
+            }
+
+            return null;
+        }
+
     }
 }
