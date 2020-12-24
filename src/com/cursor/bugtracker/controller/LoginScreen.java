082ba1a2708bfc4e490184c51f952daf8d06e394
Taras Lavreniuk <lavrt@amazon.com>
2020-12-24 15:05:06 +0100

    Create MainMenu.java

--- src/com/cursor/bugtracker/controller/LoginScreen.java
+++ src/com/cursor/bugtracker/controller/LoginScreen.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Program {
+public class LoginScreen {
 
     private static User currentUser = null;
 
---
+++ src/com/cursor/bugtracker/controller/MainMenu.java
@@ -0,0 +1,8 @@
+package com.cursor.bugtracker.controller;
+
+public class MainMenu {
+
+    public static void displayMainMenu(final String message) {
+        // TODO ...
+    }
+}
