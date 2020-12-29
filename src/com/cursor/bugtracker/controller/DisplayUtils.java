a685b5378f8892dc5561eab5238cfe299e75b715
Oleg <kandalov.o.a@gmail.com>
2020-12-29 20:34:45 +0200

    added method displayTickets

--- src/com/cursor/bugtracker/controller/DisplayUtils.java
+++ src/com/cursor/bugtracker/controller/DisplayUtils.java
@@ -1,8 +1,15 @@
 package com.cursor.bugtracker.controller;
 
+import com.cursor.bugtracker.model.Ticket;
+import com.cursor.bugtracker.service.TicketService;
 import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 public class DisplayUtils {
+
+    private static TicketService ticketService = TicketService.getInstance();
+    private static int count = 0;
 
     public static void clearScreen() {
         //Clears Screen in java
@@ -21,7 +28,17 @@ public class DisplayUtils {
         }
     }
 
-    // displayTicket(ticket)
+    public static void displayTickets() {
+        List<Ticket> allTickets = ticketService.getAllTickets();
+        while (count < allTickets.size()) {
+            System.out.println("Ticket №" + (count + 1) +
+                    System.lineSeparator() + "name - " + allTickets.get(count).getName() +
+                    System.lineSeparator() + "description - " + allTickets.get(count).getDescription() +
+                    System.lineSeparator() + "estimated time - " + allTickets.get(count).getEstimatedTime());
+            System.out.println();
+            count++;
+        }
+    }
 
     // displayTickets(ticket)
 
