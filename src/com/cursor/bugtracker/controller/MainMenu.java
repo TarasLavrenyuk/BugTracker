575990eab897c1531a8cc7090d4fec4fe737a479
Oleg <kandalov.o.a@gmail.com>
2021-01-01 16:35:33 +0200

    added method optionSelectionScreen and ticketDelete

--- src/com/cursor/bugtracker/controller/MainMenu.java
+++ src/com/cursor/bugtracker/controller/MainMenu.java
@@ -1,7 +1,6 @@
 package com.cursor.bugtracker.controller;
 
 import com.cursor.bugtracker.model.Ticket;
-import com.cursor.bugtracker.model.User;
 import com.cursor.bugtracker.service.TicketService;
 import com.cursor.bugtracker.service.UserService;
 
@@ -9,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 import static com.cursor.bugtracker.controller.LoginScreen.logOut;
 
@@ -28,18 +28,18 @@ public class MainMenu {
                 "2. Create new ticket\n" +
                 "3. Logout");
 
-       try( BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String option = reader.readLine();
             if (option.equals("1")) {
                 showAllTickets(" ");
-            }else if (option.equals("2")){
+            } else if (option.equals("2")) {
                 createNewTicket();
-            }else if (option.equals("3")){
+            } else if (option.equals("3")) {
                 logOut();
             }
-        }catch ( IOException E ){
-displayMainMenu("WRONG INPUT");
-       }
+        } catch (IOException E) {
+            displayMainMenu("WRONG INPUT");
+        }
     }
 
 
@@ -48,6 +48,61 @@ displayMainMenu("WRONG INPUT");
         List<Ticket> allTickets = ticketService.getAllTickets();
     }
 
-    public static  void createNewTicket(){ }
 
+    public static void optionSelectionScreen() throws IOException {
+        optionSelectionScreen("");
+    }
+
+    public static void optionSelectionScreen(final String message) throws IOException {
+        DisplayUtils.displayTickets();
+        System.out.println("Choose your option:"
+                + System.lineSeparator() + "1. Create new ticket"
+                + System.lineSeparator() + "2. Delete ticket"
+                + System.lineSeparator() + "3. Edit ticket");
+
+        Scanner reader = new Scanner(System.in);
+        DisplayUtils.clearScreen();
+        String option = reader.nextLine();
+        if (option.equals("1")) {
+            createNewTicket();
+        } else if (option.equals("2")) {
+            ticketDelete();
+        } else if (option.equals("3")) {
+            ticketEdit();
+        } else {
+            optionSelectionScreen("Incorrect input");
+        }
+    }
+
+    public static void createNewTicket() {
+    }
+
+    public static void ticketDelete() throws IOException {
+        ticketDelete("");
+    }
+
+    public static void ticketDelete(final String message) throws IOException {
+        List<Ticket> allTicket = ticketService.getAllTickets();
+
+        int size = allTicket.size();
+        int[] numberOfTickets = new int[size];
+        for (int i = 0; i < numberOfTickets.length; i++) {
+            numberOfTickets[i] = i + 1;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter ticket number");
+        int option = scanner.nextInt();
+
+        if (allTicket.get(option) != null) {
+            allTicket.remove(option);
+        } else {
+            throw new IOException();
+        }
+        ticketDelete("Incorrect input");
+    }
+
+    public static void ticketEdit() {
+
+    }
 }
