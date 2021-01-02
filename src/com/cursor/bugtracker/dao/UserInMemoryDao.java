659e350729883729606792cf406696b2fec511b8
Dmytro <65898747+fill19@users.noreply.github.com>
2021-01-03 00:56:18 +0200

    Update UserInMemoryDao.java

--- src/com/cursor/bugtracker/dao/UserInMemoryDao.java
+++ src/com/cursor/bugtracker/dao/UserInMemoryDao.java
@@ -7,30 +7,43 @@ import java.util.*;
 
 public class UserInMemoryDao implements UserDao, Singleton {
 
-    private static UserInMemoryDao instance;
+    static private UserInMemoryDao instance;
 
-    public static UserInMemoryDao getInstance(){
-        if(instance == null)
+    public static UserInMemoryDao getInstance() {
+        if (instance == null) {
             instance = new UserInMemoryDao();
+        }
         return instance;
     }
 
     private Map<String, User> users = new HashMap<>();
 
-    public UserInMemoryDao() {
+    private UserInMemoryDao() {
         String id1 = "5a817bdd-8294-4b05-a8f6-dc0e631563e5";
         User user1 = new User(id1, "taras", "password");
 
-        String id2 = "a6a749cb-8e8b-4471-a63c-c59004a7ccf5";
-        User user2 = new User(id1, "john", "password");
+        String id2 = "7c2a397b-3634-426f-bf57-4a9bda5d6ede";
+        User user2 = new User(id2, "john", "password");
+
+        String id3 = "4d87fd3a-ae9c-4834-a166-c745a8c92cda ";
+        User user3 = new User(id3, "jake", "password");
+
+        String id4 = "9e7c6150-54f4-4f9e-a706-1412d65eb03a";
+        User user4 = new User(id4, "frank", "password");
+
+        String id5 = "be3c0e68-d35c-4960-9c88-82f789bbcd06";
+        User user5 = new User(id5, "kyle", "password");
 
         users.put(id1, user1);
         users.put(id2, user2);
+        users.put(id3, user3);
+        users.put(id4, user4);
+        users.put(id5, user5);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return new ArrayList<>(users.values());
     }
 
     @Override
@@ -45,25 +58,32 @@ public class UserInMemoryDao implements UserDao, Singleton {
     }
 
     @Override
-    public User deleteUserById(String userId) {
-        return users.remove(userId);
+    public boolean deleteUserById(String userId) {
+        // TODO;
+        users.remove(userId);
+        return Boolean.parseBoolean(userId);
+    }
+
+    // TODO;
+    public List<User> findByName(String query) {
+        final List<User> result = new LinkedList<>();
+        for (User users : result) {
+            if (users.getUsername().equals(query)) {
+                return result;
+            }
+            // find users
+        }
+
+        return null;
     }
 
     @Override
     public User getUserByUsername(String username) {
         for (User user : getAllUsers()) {
-            if (user.getName().equals(username)) {
+            if (user.getUsername().equals(username)) {
                 return user;
             }
         }
         return null;
-    }
-
-    public List<User> findByName(String query) {
-        final List<User> result = new LinkedList<>();
-
-        //// find tickets
-
-        return result;
     }
 }
