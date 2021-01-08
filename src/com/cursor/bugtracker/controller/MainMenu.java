c5e4cbd5b6008c6c664a685c0286af768325b096
Oleg <kandalov.o.a@gmail.com>
2021-01-08 23:43:36 +0200

    refactoring method delete

--- src/com/cursor/bugtracker/controller/MainMenu.java
+++ src/com/cursor/bugtracker/controller/MainMenu.java
@@ -1,18 +1,11 @@
 package com.cursor.bugtracker.controller;
-
-import com.cursor.bugtracker.enums.Priority;
-import com.cursor.bugtracker.enums.Status;
-import com.cursor.bugtracker.exceptions.InvalidTicketNameException;
 import com.cursor.bugtracker.exceptions.TicketNotFoundException;
-import com.cursor.bugtracker.exceptions.UserNotFoundException;
 import com.cursor.bugtracker.model.Ticket;
 import com.cursor.bugtracker.service.TicketService;
 import com.cursor.bugtracker.service.UserService;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -68,15 +61,9 @@ public class MainMenu {
         if (option.equals("1")) {
             TicketOperationsScreen.createNewTicket();
         } else if (option.equals("2")) {
-            try {
-                deleteTicket();
-            } catch (IOException e) {
-                showTicketMenu("Oops, something went wrong. Please try again.");
-            } catch (TicketNotFoundException e) {
-                System.out.println(e.getMessage());
-            }
+            deleteTicket();
         } else if (option.equals("3")) {
-            editTicket();
+            TicketOperationsScreen.editTicket();
         } else if (option.equals("4")) {
             displayMainMenu("");
         } else {
@@ -84,7 +71,7 @@ public class MainMenu {
         }
     }
 
-    public static void deleteTicket() throws IOException, TicketNotFoundException {
+    public static void deleteTicket() {
         deleteTicket("");
     }
 
@@ -113,85 +100,6 @@ public class MainMenu {
         showTicketMenu();
     }
 
-    public static void editTicket() {
-        editTicket("");
-    }
-
-    public static void editTicket(final String message) {
-        final Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the ticket number you want to edit");
-        List<Ticket> tickets = ticketService.getAllTickets();
-        displayTickets(tickets);
-        int numberTicket = scanner.nextInt() - 1;
-
-        if (numberTicket < 0 || numberTicket > tickets.size() - 1) {
-            editTicket("Incorrect input");
-        }
-        Ticket ticket = tickets.get(numberTicket);
-
-        System.out.println("Enter new name of ticket, or skip the step: ");
-        String name = ticket.getName();
-        if (!scanner.nextLine().isBlank()) {
-            name = scanner.nextLine();
-        }
-
-        System.out.println("Enter new description, or skip the step: ");
-        String description = ticket.getDescription();
-        if (!scanner.nextLine().isBlank()) {
-            description = scanner.nextLine();
-        }
-
-        System.out.println
-                ("Enter separated by space new list the names of registered users in the list of assignee" +
-                        System.lineSeparator() + ", or skip the step: ");
-        List<String> assigneeList = ticket.getAssigneeList();
-        if (!scanner.nextLine().isBlank()) {
-            String[] tempNameLine = scanner.nextLine().split(" ");
-            assigneeList = userService.findByName(tempNameLine);
-        }
-
-        System.out.println("Enter new status of your ticket: TODO, IN_PROGRESS, IN_REVIEW, DONE" +
-                ", or skip the step:");
-        Status status = ticket.getStatus();
-        if (!scanner.nextLine().isBlank()) {
-            String selectStatus = scanner.nextLine().toUpperCase();
-            status = switch (selectStatus) {
-                case "TODO" -> Status.TODO;
-                case "IN_PROGRESS" -> Status.IN_PROGRESS;
-                case "IN_REVIEW" -> Status.IN_REVIEW;
-                case "DONE" -> Status.DONE;
-                default -> throw new IllegalStateException("Unexpected value: " + selectStatus);
-            };
-        }
-
-        System.out.println("Enter new priority of ticket: LOW, MEDIUM, HIGH, or skip the step:");
-        Priority priority = ticket.getPriority();
-        if (!scanner.nextLine().isBlank()) {
-            String selectPriority = scanner.nextLine().toUpperCase();
-            priority = switch (selectPriority) {
-                case "LOW" -> Priority.LOW;
-                case "MEDIUM" -> Priority.MEDIUM;
-                case "HIGH" -> Priority.HIGH;
-                default -> throw new IllegalStateException("Unexpected value: " + selectPriority);
-            };
-        }
-
-        try {
-            ticket = ticketService.edit(
-                    ticket.getTicketId(),
-                    name,
-                    description,
-                    assigneeList,
-                    status,
-                    priority
-            );
-        } catch (InvalidTicketNameException | UserNotFoundException | TicketNotFoundException e) {
-            e.printStackTrace();
-        }
-        finally {
-            showTicketMenu();
-        }
-    }
 
     /**
      * Show all tickets
